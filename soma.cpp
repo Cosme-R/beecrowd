0281d572c82cef92009e43915c3083bbad90f826
Cosme Ribeiro <156976610+Cosme-R@users.noreply.github.com>
2025-05-18 18:06:56 -0300

    de
    3d

---
+++ soma.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+using namespace std; 
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Etcriba su solución aquí
+    * mprpmhpr
+    */
+  
+    int a=0;
+    int b=0;
+    cin>>a;
+    cin>>b;
+    cout<<"SOMA = "<<a+b<<endl;
+
+    return 0;
+}
